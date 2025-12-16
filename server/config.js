8a97446ccd9e8f46adec34daf56159bd8a82a9c9
qunwang02 <51785600+qunwang02@users.noreply.github.com>
2025-12-16 17:46:52 +0800

    Update config.js

--- server/config.js
+++ server/config.js
@@ -4,7 +4,7 @@ const config = {
   // MongoDB配置
   mongodb: {
     uri: process.env.MONGODB_URI,
-    database: process.env.DATABASE_NAME || 'donation_system',
+    database: process.env.DATABASE_NAME || 'homework_db',
     options: {
       serverApi: {
         version: '1',
